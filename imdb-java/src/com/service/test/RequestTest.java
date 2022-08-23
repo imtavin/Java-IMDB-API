f723714e3c6b8e3200878b6f12b74cddd9109e38
Gustavo Espenchitt <gustavo.t.espenchitt@gmail.com>
2022-08-23 14:20:20 -0300

    Adicionando requisicao com IMDb

---
+++ imdb-java/src/com/service/test/RequestTest.java
@@ -0,0 +1,14 @@
+package com.service.test;
+
+import com.service.imdb.Request;
+import com.service.imdb.impresaoJson;
+
+public class RequestTest {
+
+	public void retornaJsonImdb (){
+		
+		
+		
+	}
+
+}
